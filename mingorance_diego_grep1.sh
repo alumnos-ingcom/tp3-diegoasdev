c92c1cac6ab2d17dc7d7a9996dcadf02419788a3
diegoasdev <83685135+diegoasdev@users.noreply.github.com>
2021-05-23 22:56:52 -0300

    Added primer consigna

--- mingorance_diego_grep1.sh
+++ mingorance_diego_grep1.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+# La variable FILE recibe el nombre del archivo a utilizar por los comandos grep siguientes, 
+#   o define grepdata.txt en forma predeterminada si no se especifica
+FILE=${1:-grepdata.txt}
+
+echo
+echo "+++ Consigna 1 +++"
+grep -E "[[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4} [xX][[:digit:]]{4}" $FILE
+echo
